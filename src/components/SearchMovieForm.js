89d6b692defbac0792d4c3b618bdb4f2cc733612
Dominika Duda <duda.dominika97@gmail.com>
2021-05-15 20:04:23 +0200

    update templates

--- src/components/SearchMovieForm.js
+++ src/components/SearchMovieForm.js
@@ -26,9 +26,6 @@ export default class SearchMovieForm extends React.Component {
         } else {
             this.setState(() => ({ error: '' }));
             //wyszukanie info z omdbAPI
-            this.props.onSubmit({
-                title: this.state.title
-            });
         }
 
         alert('Title ' + this.state.title + ' submitted!');
@@ -36,20 +33,31 @@ export default class SearchMovieForm extends React.Component {
 
     render() {
         return (
-            <form className="form" onSubmit={this.onTitleSubmit}>
-                {this.state.error && <p className="form__error">{this.state.error}</p>}
-                <input
-                    type="text"
-                    placeholder="Type title to search..."
-                    autoFocus
-                    className="text-input"
-                    value={this.state.title}
-                    onChange={this.onTitleChange}
-                />
-                <div>
-                    <button className="button">Search Movie</button>
+            <div className="page-header">
+                    <div className="content-container">
+                        <h1 className="page-header__title">Search for Movie</h1>
+                    </div>
+                    <div className="content-container">
+                        <form className="form" onSubmit={this.onTitleSubmit}>
+                            {this.state.error && <p className="form__error">{this.state.error}</p>}
+                            <input
+                                type="text"
+                                placeholder="Type title to search..."
+                                autoFocus
+                                className="text-input"
+                                value={this.state.title}
+                                onChange={this.onTitleChange}
+                            />
+                            <div>
+                            <button className="button">Search Movie</button>
+                            </div>
+                        </form>
+                    </div>
+                <div className="content-container">
+                    Wyszukane filmy?
                 </div>
-            </form>
+            </div>
+
         )
     }
 }
---
+++ src/utils/omdbAPI.js
@@ -0,0 +1,23 @@
+const request = require('request')
+
+const omdbAPI = (title, callback) => {
+    const url = 'https://www.omdbapi.com/?t=' + encodeURIComponent(title) + '&apikey=' + process.env.OMDB_API_KEY
+
+    request({ url, json: true }, (error, response ) => {
+        if (error) {
+            callback('Unable to connect to movie services!', undefined)
+        } else if (response.body.Error) {
+            callback('Unable to find movie. Try again with different search term', undefined)
+        } else {
+            callback(undefined, {
+                Title: response.body.Title,
+                Year: response.body.Year,
+                Genre: response.body.Genre,
+                imdbID: response.body.imdbID,
+                poster: response.body.poster
+            })
+        }
+    })
+}
+
+module.exports = omdbAPI
